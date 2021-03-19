9749496954c33f559bd4c9c5a7c75dc7e8de6421
madsur <dennissheets@outlook.com>
2021-03-19 09:16:07 -0500

    added sort code to line 7. We ran out of time and forgot to add it

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 window.addEventListener("load", ()=> {
     fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response){
         response.json().then(function(json){
-            //console.log(json);
-        const container = document.getElementById("container");
+            
+            const container = document.getElementById("container");
 
-        json = json.sort(function(a, b){return b.hoursInSpace-a.hoursInSpace}); // added this line to sort the json for the bonus.
+            json = json.sort(function(a, b){return b.hoursInSpace-a.hoursInSpace}); // added this line to sort the json for the bonus.
 
-        container.innerHTML=`<p>Total Astronauts: ${json.length}</p>`
-         
-        for (let i=0; i<json.length; i++) {
-            let color = "red";
-            if (json[i].active) {color = "green"
-            }
-            container.innerHTML += `
-                <div class="astronaut">
-                <div class="bio">
-                <h3>${json[i].firstName} ${json[i].lastName}</h3>
-                <ul>
-                    <li>Hours in space: ${json[i].hoursInSpace}</li>
-                    <li style="color:${color}">Active: ${json[i].active}</li>
-                    <li>Skills: ${json[i].skills}</li>
-                </ul>
-                </div>
-                <img class="avatar" src="${json[i].picture}">
-            </div>
-                `;
-        }
-        
+            container.innerHTML=`<p>Total Astronauts: ${json.length}</p>`
+            
+            for (let i=0; i<json.length; i++) {
+                
+                let color = "red";
+                if (json[i].active) {color = "green"
+                }
+
+                container.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li style="color:${color}">Active: ${json[i].active}</li>
+                                <li>Skills: ${json[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar"S src="${json[i].picture}">
+                    </div>
+                `
+            };
         });
     });
 });
